24fb04bbfe67c39e21a42dd6d2c069e1c6b2eb62
mataiodoxion <lucasmasterson.edu@protonmail.com>
2025-10-27 14:20:37 -0700

    Add WASMs

---
+++ pkg/crypto_utils/crypto_utils_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const add: (a: number, b: number) => number;
+export const multiply: (a: number, b: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
